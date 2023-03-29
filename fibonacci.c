9a5f807bfbfee7ccbd9f7f66057288ff8433d892
RLN <113087232+RLN2004@users.noreply.github.com>
2023-03-29 14:36:42 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
+
 int fib(int n){
 if(n==1)
 return 0;
@@ -7,6 +8,7 @@ if(n==2)
 return 1;
 return fib(n-1)+fib(n-2);
 }
+
 void main(){
 int i,n;
 printf("ENTER THE VALUE OF N : ");
